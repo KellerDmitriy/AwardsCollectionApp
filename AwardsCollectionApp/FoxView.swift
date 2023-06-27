caccc3709c0a818ad40ebb4e271deca744742967
DmKeller <dmit.keller@gmail.com>
2023-06-27 23:58:05 +0300

    add shape

--- AwardsCollectionApp/FoxView.swift
+++ AwardsCollectionApp/FoxView.swift
@@ -7,75 +7,61 @@
 
 import SwiftUI
 
-struct FoxView: View {
+struct BannerView: View {
     var body: some View {
-        GeometryReader { geometry in
-            let width = geometry.size.width
-            let height = geometry.size.height
-
-            ZStack {
-                ForEach(0..<3) { iteration in
-                    AwaShape()
-                        .fill(
-                            LinearGradient(
-                                gradient: Gradient(colors: [.green, .blue]),
-                                startPoint: UnitPoint(x: 0, y: 1),
-                                endPoint: UnitPoint(x: 1, y: 0)
-                            )
-                        )
-                        .rotationEffect(.degrees(Double(iteration) * 60))
-                        .frame(width: width * 0.7, height: height * 0.7)
-                }
-                Image(systemName: "airplane")
-                    .resizable()
-                    .rotationEffect(.degrees(-90))
-                    .opacity(0.5)
-                    .scaleEffect(0.7)
-            }
+        ZStack {
+            BannerShape()
+                .fill(Color.accentColor)
+                .ignoresSafeArea()
+            Text("Banner Shape")
+                .font(.largeTitle).bold()
+                .foregroundColor(Color.accentColor)
         }
     }
 }
 
-struct FoxView_Previews: PreviewProvider {
+struct BannerShape: Shape {
+    func path(in rect: CGRect) -> Path {
+        let width = rect.width
+        let height = rect.height
+        
+        return Path { path in
+            // Top drawing
+            path.move(to: .zero)
+            path.addLine(to: CGPoint(x: rect.width, y: 0))
+            path.addLine(to: CGPoint(x: rect.width, y: rect.height*0.3125))
+            path.addCurve(to: CGPoint(x: 40, y: 75),
+                          control1: CGPoint(x: rect.width-40, y: rect.height*0.15625),
+                          control2: CGPoint(x: rect.width*3/5, y: rect.height*0.09375))
+            path.addCurve(to: CGPoint(x: 0, y: 50),
+                          control1: CGPoint(x: 10, y: 75),
+                          control2: CGPoint(x: 0, y: 65))
+            path.addLine(to: .zero)
+            
+            path.move(to: CGPoint(x: 0, y: rect.height*0.6875))
+            
+            // Close subpath so you can draw the next path
+            path.closeSubpath()
+            
+            // Bottom drawing
+            path.addCurve(to: CGPoint(x: width-40, y: height-75),
+                          control1: CGPoint(x: 40, y: height*0.84375),
+                          control2: CGPoint(x: width*2/5, y: height*0.90625))
+            
+            path.addCurve(to: CGPoint(x: width, y: height-50),
+                          control1: CGPoint(x: width-10, y: height-75),
+                          control2: CGPoint(x: width, y: height-65))
+            path.addLine(to: CGPoint(x: width, y: height))
+            path.addLine(to: CGPoint(x: 0, y: height))
+            
+            path.addLine(to: CGPoint(x: 0, y: rect.height*0.6875))
+            path.closeSubpath()
+        }
+    }
+}
+
+struct BannerView_Previews: PreviewProvider {
     static var previews: some View {
-        FoxView()
-            .frame(width: 200, height: 200)
-    }
-}
-
-struct AwaShape: View {
-    
-    let symbolColor = Color(red: 79.0 / 255, green: 79.0 / 255, blue: 191.0 / 255)
-    
-    var body: some View {
-        GeometryReader { geometry in
-            Path { path in
-                let width = min(geometry.size.width, geometry.size.height)
-                let height = width * 0.75
-                let spacing = width * 0.030
-                let middle = width * 0.5
-                let topWidth = width * 0.226
-                let topHeight = height * 0.488
-                
-                
-                path.addLines([
-                    CGPoint(x: middle, y: spacing),
-                    CGPoint(x: middle - topWidth, y: topHeight - spacing),
-                    CGPoint(x: middle, y: topHeight / 2 + spacing),
-                    CGPoint(x: middle + topWidth, y: topHeight - spacing),
-                    CGPoint(x: middle, y: spacing)
-                ])
-                
-                path.move(to: CGPoint(x: middle, y: topHeight / 2 + spacing * 3))
-                path.addLines([
-                    CGPoint(x: middle - topWidth, y: topHeight + spacing),
-                    CGPoint(x: spacing, y: height - spacing),
-                    CGPoint(x: width - spacing, y: height - spacing),
-                    CGPoint(x: middle + topWidth, y: topHeight + spacing),
-                    CGPoint(x: middle, y: topHeight / 2 + spacing * 3)
-                ])
-            }
-            .fill(symbolColor)
-        }
+        BannerView()
     }
 }
